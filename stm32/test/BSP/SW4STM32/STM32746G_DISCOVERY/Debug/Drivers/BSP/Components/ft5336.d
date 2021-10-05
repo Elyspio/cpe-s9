562270514a3fda51094cf51f5c7db28eeb3969ee
Jonathan Guichard <jona.guich69@gmail.com>
2021-10-05 12:58:31 +0200

    STM 32 - Clean

--- stm32/test/BSP/SW4STM32/STM32746G_DISCOVERY/Debug/Drivers/BSP/Components/ft5336.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/ft5336.o: \
- /media/projects/cpe/s9/environnements-intelligents-communicants/stm32/test/BSP/Drivers/BSP/Components/ft5336/ft5336.c \
- /media/projects/cpe/s9/environnements-intelligents-communicants/stm32/test/BSP/Drivers/BSP/Components/ft5336/ft5336.h \
- /media/projects/cpe/s9/environnements-intelligents-communicants/stm32/test/BSP/Drivers/BSP/Components/ft5336/../Common/ts.h
-
-/media/projects/cpe/s9/environnements-intelligents-communicants/stm32/test/BSP/Drivers/BSP/Components/ft5336/ft5336.h:
-
-/media/projects/cpe/s9/environnements-intelligents-communicants/stm32/test/BSP/Drivers/BSP/Components/ft5336/../Common/ts.h:
--- stm32/test/BSP/SW4STM32/STM32746G_DISCOVERY/Debug/Example/SW4STM32/syscalls.d
+++
@@ -1,2 +0,0 @@
-Example/SW4STM32/syscalls.o: \
- /media/projects/cpe/s9/environnements-intelligents-communicants/stm32/test/BSP/SW4STM32/syscalls.c
